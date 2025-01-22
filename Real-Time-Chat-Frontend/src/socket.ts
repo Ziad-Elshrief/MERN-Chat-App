7868d4952052b6f53acf1ffd4cf0e8989e0589b1
Ziad Elshrief <zyad2100@gmail.com>
2025-01-22 18:36:47 +0200

    fix: fixed socket io origin in client

--- Real-Time-Chat-Frontend/src/socket.ts
+++ Real-Time-Chat-Frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket =
-  import.meta.env.NODE_ENV === "production"
-    ? io()
-    : io("http://localhost:3000/");
+export const socket = io()
+
+// development
+//export const socket =  io("http://localhost:3000/");
